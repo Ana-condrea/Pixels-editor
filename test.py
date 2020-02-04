d4d858923ca0a62e1013da4811ac61a2836af296
Ana-condrea <ana.condrea228@yahoo.com>
2020-02-04 17:45:56 +0200

    Refactoring the structure.

--- test.py
+++
@@ -1,8 +0,0 @@
-import soundfile as sf
-import numpy as np
-sound_path = "voice.wav"
-maxim = 32767
-
-data, samplerate = sf.read(sound_path)
-scaled = np.int16(data/np.max(np.abs(data)) * maxim)
-print(len(scaled))
